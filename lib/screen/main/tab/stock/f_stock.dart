692689eb84969bb6e96b555eb60b0052fc49a69d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-26 19:50:00 +0900

    Created app bar in stock fragment.

--- lib/screen/main/tab/stock/f_stock.dart
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:toss/common/common.dart';
+import 'package:toss/common/widget/w_icon_button.dart';
 
 class StockFragment extends StatefulWidget {
+  final double iconSize = 30.0;
+  final double iconPadding = 10.0;
   const StockFragment({super.key});
 
   @override
@@ -10,6 +14,35 @@ class StockFragment extends StatefulWidget {
 class _StockFragmentState extends State<StockFragment> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: context.appColors.appBarBackground,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: context.appColors.appBarBackground,
+            actions: [
+              IconsButton(
+                icon: Icons.search,
+                onTap: () {
+                  context.showSnackbar("검색");
+                },
+              ),
+              IconsButton(
+                icon: Icons.edit_calendar_outlined,
+                onTap: () {
+                  context.showSnackbar("캘린더");
+                },
+              ),
+              IconsButton(
+                icon: Icons.settings,
+                onTap: () {
+                  context.showSnackbar("설정");
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
