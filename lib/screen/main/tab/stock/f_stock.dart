43adf05fd89ca2425512a435ad8f980c696a4900
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-26 21:34:01 +0900

    Created my stock & todays discovery tab.

--- lib/screen/main/tab/stock/f_stock.dart
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:toss/common/common.dart';
 import 'package:toss/common/widget/w_icon_button.dart';
+import 'package:toss/screen/main/tab/stock/tab/f_my_stock.dart';
+import 'package:toss/screen/main/tab/stock/tab/f_todays_discovery.dart';
 
 class StockFragment extends StatefulWidget {
   final double iconSize = 30.0;
@@ -11,7 +13,11 @@ class StockFragment extends StatefulWidget {
   State<StockFragment> createState() => _StockFragmentState();
 }
 
-class _StockFragmentState extends State<StockFragment> {
+class _StockFragmentState extends State<StockFragment>
+    with SingleTickerProviderStateMixin {
+  late final tabController = TabController(length: 2, vsync: this);
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +26,8 @@ class _StockFragmentState extends State<StockFragment> {
         slivers: [
           SliverAppBar(
             backgroundColor: context.appColors.appBarBackground,
+            surfaceTintColor: context.appColors.appBarBackground,
+            pinned: true,
             actions: [
               IconsButton(
                 icon: Icons.search,
@@ -40,9 +48,65 @@ class _StockFragmentState extends State<StockFragment> {
                 },
               ),
             ],
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                title,
+                tabBar,
+                if (currentIndex == 0)
+                  const MyStockFragment()
+                else
+                  const TodaysDiscoveryFragment()
+              ],
+            ),
           )
         ],
       ),
     );
   }
+
+  Widget get title => Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          "토스증권".text.size(24).bold.make(),
+          width20,
+          'S&P 500'
+              .text
+              .size(13)
+              .bold
+              .color(context.appColors.lessImportant)
+              .make(),
+          width5,
+          3919.29
+              .toComma()
+              .text
+              .size(14)
+              .bold
+              .color(context.appColors.plus)
+              .make(),
+        ],
+      ).pOnly(left: 20);
+
+  Widget get tabBar => Column(
+        children: [
+          TabBar(
+            onTap: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            labelStyle:
+                const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            labelPadding: const EdgeInsets.symmetric(vertical: 20),
+            indicatorPadding: const EdgeInsets.symmetric(horizontal: -20),
+            indicatorColor: Colors.white,
+            tabs: ["내 주식".text.make(), "오늘의 발견".text.make()],
+            controller: tabController,
+          ),
+          const Line(),
+        ],
+      );
+  Widget get myAccount => const Placeholder();
+  Widget get myStocks => const Placeholder();
 }
---
+++ lib/screen/main/tab/stock/tab/f_my_stock.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyStockFragment extends StatelessWidget {
+  const MyStockFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      height: 500,
+    );
+  }
+}
