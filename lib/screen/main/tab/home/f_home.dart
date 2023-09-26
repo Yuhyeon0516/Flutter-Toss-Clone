02f6faa1d34487654ae30cf9c3c4dc87462c1d72
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-26 17:16:33 +0900

    Created app bar in home fragment.

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toss/common/common.dart';
-import 'package:toss/common/widget/round_button_theme.dart';
-import 'package:toss/common/widget/w_round_button.dart';
 import 'package:toss/screen/dialog/d_message.dart';
+import 'package:toss/screen/main/tab/home/w_toss_app_bar.dart';
 
 import '../../../dialog/d_color_bottom.dart';
 import '../../../dialog/d_confirm.dart';
@@ -15,44 +14,15 @@ class HomeFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: context.appColors.seedColor.getMaterialColorValues[100],
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+      color: Colors.black,
+      child: const Stack(
         children: [
-          Row(
-            children: [
-              IconButton(
-                onPressed: () => openDrawer(context),
-                icon: const Icon(Icons.menu),
-              )
-            ],
+          SingleChildScrollView(
+            child: Column(
+              children: [],
+            ),
           ),
-          const EmptyExpanded(),
-          RoundButton(
-            text: 'Snackbar 보이기',
-            onTap: () => showSnackbar(context),
-            theme: RoundButtonTheme.blue,
-          ),
-          const Height(20),
-          RoundButton(
-            text: 'Confirm 다이얼로그',
-            onTap: () => showConfirmDialog(context),
-            theme: RoundButtonTheme.whiteWithBlueBorder,
-          ),
-          const Height(20),
-          RoundButton(
-            text: 'Message 다이얼로그',
-            onTap: showMessageDialog,
-            theme: RoundButtonTheme.whiteWithBlueBorder,
-          ),
-          const Height(20),
-          RoundButton(
-            text: '메뉴 보기',
-            onTap: () => openDrawer(context),
-            theme: RoundButtonTheme.blink,
-          ),
-          const EmptyExpanded()
+          TossAppBar()
         ],
       ),
     );
