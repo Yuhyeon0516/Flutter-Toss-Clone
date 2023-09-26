6c4d45bbf3f731e204416c028dbf6d218a486b46
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-26 23:00:31 +0900

    Added comma in home screen price.

--- lib/screen/main/tab/home/bank_accounts_dummy.dart
+++ lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -2,13 +2,13 @@ import 'package:toss/screen/main/tab/home/banks_dummy.dart';
 import 'package:toss/screen/main/tab/home/vo/vo_bank_account.dart';
 
 final bankAccountShinhan1 =
-    BankAccount(bankShinhan, 3000000, accountTypeName: '신한 주거래 우대통장(저축예금)');
+    BankAccount(bankShinhan, 12758295671, accountTypeName: '신한 주거래 우대통장(저축예금)');
 final bankAccountShinhan2 =
-    BankAccount(bankShinhan, 30000000, accountTypeName: '저축예금');
+    BankAccount(bankShinhan, 968384717, accountTypeName: '저축예금');
 final bankAccountShinhan3 =
-    BankAccount(bankShinhan, 300000000, accountTypeName: '저축예금');
-final bankAccountToss = BankAccount(bankToss, 50000);
-final bankAccountKakao = BankAccount(bankKakao, 2000000);
+    BankAccount(bankShinhan, 5839571, accountTypeName: '저축예금');
+final bankAccountToss = BankAccount(bankToss, 3230);
+final bankAccountKakao = BankAccount(bankKakao, 783947917);
 
 final bankAccounts = [
   bankAccountShinhan1,
